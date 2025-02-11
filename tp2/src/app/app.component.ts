cb8292abb1e35c664a773a5530efce189f620356
Regan SASIKUMAR <rsasikumar@a101-16.iut.univ-paris8.fr>
2025-02-11 15:36:42 +0100

    header fait + composant footer crée

--- tp2/src/app/app.component.ts
+++ tp2/src/app/app.component.ts
@@ -1,21 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'tp2';
+  title = 'Trafalgar';
 }
 
-
-type Mc = {
-  tag : "S1" | "S2" | "S3" | "S4" | "S5" | "S6" | "Ressource";
-  imgUrl : String;
-  link : String;
-
-}
